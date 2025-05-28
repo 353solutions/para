1e14f887e7ffed7de9093ceecef8e8a4a0080bba
Miki Tebeka <miki@353solutions.com>
2025-05-28 11:41:19 +0300

    insert

--- cars/httpd.go
+++ cars/httpd.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	// _ "expvar" // for side effect of registering in default mux
+	"encoding/json"
 	"expvar"
 	"fmt"
 	"log/slog"
@@ -23,17 +24,31 @@ CRUD -> REST HTTP Verbs
 - Delete: DELETE
 */
 
+/* Install go 1.24
+$ ./_extra/update-go 1.24.3
+$ ~/go/bin/go1.24.3 verion
+$ alias go=~/go/bin/go1.24.3
+*/
+
 func main() {
 	log := slog.Default().With("app", "cars")
+	db, err := NewDB("cars.db")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: create db - %s\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	api := API{
 		log: log, // dependency injection
+		db:  db,
 	}
 	// Routing
 	// Will use default router (mux)
 	// http.HandleFunc("GET /health", healthHandler)
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /health", api.healthHandler)
+	mux.HandleFunc("POST /rides", api.insertHandler)
 	mux.Handle("GET /debug/vars", expvar.Handler())
 
 	srv := http.Server{
@@ -51,12 +66,49 @@ func main() {
 	}
 }
 
-type API struct {
-	log *slog.Logger
+func (a *API) insertHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse + Validate data
+	var e Event
+	if err := json.NewDecoder(r.Body).Decode(&e); err != nil {
+		a.log.Error("bad request", "error", err)
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+	if err := e.Validate(); err != nil {
+		a.log.Error("invalid event", "error", err)
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	// Work
+	if err := a.db.Insert(e); err != nil {
+		a.log.Error("insert", "error", err)
+		http.Error(w, "can't insert", http.StatusInternalServerError)
+		return
+	}
+
+	// Output
+	resp := map[string]any{
+		"id": e.ID,
+	}
+	if err := writeJSON(w, resp); err != nil {
+		// Can't send to client
+		a.log.Error("write JSON", "error", err)
+	}
+}
+
+func writeJSON(w http.ResponseWriter, resp any) error {
+	w.Header().Set("content-type", "application/json")
+	return json.NewEncoder(w).Encode(resp)
 }
 
 func (a *API) healthHandler(w http.ResponseWriter, r *http.Request) {
 	a.log.Info("health", "path", r.URL.Path)
 	// TODO: Real health check
 	fmt.Fprintln(w, "OK")
+}
+
+type API struct {
+	log *slog.Logger
+	db  *DB
 }
