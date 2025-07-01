c733e92b61d5fb0f5bc89eb67b2c1330f2d2a231
Miki Tebeka <miki@353solutions.com>
2025-07-01 15:14:12 +0300

    go

---
+++ go_chan/go_chan.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"sync"
+	"time"
+)
+
+/*
+Concurrency: Dealing with several things at the same time
+Parallelism: Doing with several things at the same time
+*/
+
+/* Channel semantics
+- send/receive will block until opposite operation(*)
+	- guarantee of delivery
+- receive from a closed channel will return zero value without blocking
+*/
+
+/* Concurrency patterns
+fanOut - Split work to goroutines
+	- Wait to finish (sync.WaitGroup)
+	- Check for error (errgroup)
+	- Get return value(s)
+*/
+
+func main() {
+	go fmt.Println("goroutine")
+	fmt.Println("main")
+
+	// This is a bug in Go < 1.22(?)
+	for i := range 3 {
+		// i := i
+		go func() {
+			fmt.Println("i:", i)
+		}()
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	ch := make(chan string)
+	go func() {
+		ch <- "hi" // send
+	}()
+	msg := <-ch // receive
+	fmt.Println("msg:", msg)
+
+	go func() {
+		for i := range 5 {
+			msg := fmt.Sprintf("msg %d", i)
+			ch <- msg
+		}
+		close(ch)
+	}()
+
+	for msg := range ch {
+		fmt.Println(msg)
+	}
+
+	/* What the above "for .. range" does
+
+	for {
+		msg, ok := <-ch
+		if !ok {
+			break
+		}
+		fmt.Println(msg)
+	}
+	*/
+
+	msg = <-ch // ch is closed
+	fmt.Printf("msg: %q\n", msg)
+	// zero vs closed?
+	msg, ok := <-ch
+	fmt.Printf("msg: %q (ok=%v)\n", msg, ok)
+
+	urls := []string{
+		"https://go.dev",
+		"https://google.com",
+		"https://ibm.com/no/such/page",
+	}
+
+	start := time.Now()
+
+	var wg sync.WaitGroup
+	wg.Add(len(urls))
+	for _, url := range urls {
+		// wg.Add(1)
+		go func() {
+			defer wg.Done()
+			urlInfo(url)
+		}()
+	}
+
+	wg.Wait()
+	duration := time.Since(start)
+	fmt.Printf("%d URLs in %v\n", len(urls), duration)
+}
+
+func urlInfo(url string) {
+	//	s := time.Now()
+	resp, err := http.Get(url)
+	//	fmt.Printf("%q: %v\n", url, time.Since(s))
+
+	if err != nil {
+		slog.Error("info", "url", url, "error", err)
+	}
+	slog.Info("info", "url", url, "status", resp.Status)
+}
--- playground/playground.go
+++ playground/playground.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
 
 func main() {
-	a, b := 1.1, 2.2
-	fmt.Println(a + b)
+	/*
+		a, b := 1.1, 2.2
+		fmt.Println(a + b)
+	*/
+
+	data := `
+	{"x": 1, "y": 2}
+	{"x": 3, "y": 4}
+	`
+	dec := json.NewDecoder(strings.NewReader(data))
+loop:
+	for {
+		var m map[string]int
+		err := dec.Decode(&m)
+		switch {
+		case errors.Is(err, io.EOF):
+			break loop
+		case err != nil:
+			fmt.Println("ERROR:", err)
+			break loop
+		}
+		fmt.Println(m)
+	}
 }
