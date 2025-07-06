a2d20a5472cf0c7093296ba70b5b061f86540bd1
Miki Tebeka <miki@353solutions.com>
2025-07-06 12:14:52 +0300

    pmap

--- pmap/pmap.go
+++ pmap/pmap.go
@@ -8,12 +8,12 @@ import (
 func main() {
 	start := time.Now()
 	vals := PMap(int2str, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3)
-	fmt.Printf("%#v\n", vals) // ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+	fmt.Printf("%#v\n", vals) // []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 	fmt.Println(time.Since(start))
 
 	start = time.Now()
 	iVals := PMap(inc, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3)
-	fmt.Printf("%#v\n", iVals) // [2, 3, 4, 5, 6, 7, 8, 9, 10]
+	fmt.Printf("%#v\n", iVals) // [int]{2, 3, 4, 5, 6, 7, 8, 9, 10}
 	fmt.Println(time.Since(start))
 
 }
@@ -29,7 +29,7 @@ func int2str(i int) string {
 }
 
 // PMap will run concurrently "fn" on every element of values.
-// It will run up to "n" goroutines at the same time
+// It will run up to "n" goroutines at the same time.
 // Use golang.org/x/sync/semaphore
 func PMap[V any, R any](fn func(V) R, values []V, n int) []R {
 	return nil // FIXME:
