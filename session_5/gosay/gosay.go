efc6329827dfce877ba12745f5501b82236b8071
Miki Tebeka <miki@353solutions.com>
2025-09-17 16:09:05 +0300

    cgo

--- session_5/gosay/gosay.go
+++ session_5/gosay/gosay.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"strings"
+	"time"
 
 	// _ "embed" // if you only use //go:embed directives
 	"embed"
@@ -17,10 +22,12 @@ var version = "<dev>"
 
 var options struct {
 	showVersion bool
+	cow         bool
 }
 
 func main() {
 	flag.BoolVar(&options.showVersion, "version", false, "show version & exit")
+	flag.BoolVar(&options.cow, "cow", false, "use cowsay API")
 	flag.Usage = func() {
 		prog := path.Base(os.Args[0])
 		fmt.Fprintf(os.Stderr, "usage: %s TEXT\n", prog)
@@ -40,6 +47,11 @@ func main() {
 	}
 
 	text := flag.Arg(0)
+	if options.cow {
+		cow(text)
+		os.Exit(0)
+	}
+
 	width := len(text)
 	fmt.Printf("  %s\n", strings.Repeat("-", width))
 	fmt.Printf("< %s >\n", text)
@@ -53,10 +65,50 @@ var gopher string
 //go:embed *
 var assets embed.FS
 
-/* Usage for embed
+/*
+	Usage for embed
+
 - Embedded type can be string, []byte or embed.FS
 - Static assets for web servers (js, css ...)
-	- See https://pkg.go.dev/embed#example-package and http.StripPrefix
-	- You can embed text/html templates but need to compile them at startup
+  - See https://pkg.go.dev/embed#example-package and http.StripPrefix
+  - You can embed text/html templates but need to compile them at startup
+
 - SQL files
+*/
+// Exercise: Add a --image flag that will use image from the images directory
+// If not specified use gopher.txt
+// Embed the images directory to the executable
+func cow(text string) error {
+	q := url.Values{}
+	q.Add("message", text)
+	q.Add("format", "text")
+	url := "https://cowsay.morecode.org/say?" + q.Encode()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("%q - %s", url, resp.Status)
+	}
+
+	_, err = io.Copy(os.Stdout, resp.Body)
+	fmt.Println()
+	return err
+}
+
+/*
+$ GOOS=darwin GOARCH=arm64 go build
+$ go tool dist list
+$ ldd gosay (Linux)
+$ otool -L (OSX)
 */
