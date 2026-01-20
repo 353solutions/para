d78e6e1a19a927fefebbac6db065f5aeef2b72ac
Miki Tebeka <miki@353solutions.com>
2026-01-20 11:45:00 +0200

    install

--- session_6/unter/backoffice_test.go
+++ session_6/unter/backoffice_test.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"os"
 	"testing"
+
+	"github.com/goccy/go-yaml"
 )
 
-func TestRidePrice(t *testing.T) {
-	// TODO: Load test cases from testdata/price_test.yml
-	for _, tt := range priceTests {
+type priceTest struct {
+	Name     string  `yaml:"name"`
+	Distance float64 `yaml:"distance"`
+	Shared   bool    `yaml:"shared"`
+	Want     int     `yaml:"want"`
+}
 
-		t.Run(tt.name, func(t *testing.T) {
-			got := RidePrice(tt.distance, tt.shared)
-			if got != tt.want {
-				t.Errorf("RidePrice(%v, %v) = %v, want %v", tt.distance, tt.shared, got, tt.want)
+func TestRidePrice(t *testing.T) {
+	data, err := os.ReadFile("testdata/price_tests.yml")
+	if err != nil {
+		t.Fatalf("failed to read test file: %v", err)
+	}
+
+	var priceTests []priceTest
+	if err := yaml.Unmarshal(data, &priceTests); err != nil {
+		t.Fatalf("failed to parse test file: %v", err)
+	}
+
+	for _, tt := range priceTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			got := RidePrice(tt.Distance, tt.Shared)
+			if got != tt.Want {
+				t.Errorf("RidePrice(%v, %v) = %v, want %v", tt.Distance, tt.Shared, got, tt.Want)
 			}
 		})
 	}
--- session_6/unter/go.mod
+++ session_6/unter/go.mod
@@ -2,7 +2,10 @@ module unter
 
 go 1.25
 
-require go.etcd.io/bbolt v1.4.3
+require (
+	github.com/goccy/go-yaml v1.19.2
+	go.etcd.io/bbolt v1.4.3
+)
 
 require (
 	github.com/stretchr/testify v1.11.1 // indirect
